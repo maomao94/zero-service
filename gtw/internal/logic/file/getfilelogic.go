d7460a6a560cb7631629a0bdffbaa184859b1c56
hehanpeng <hehanpengyy@163.com>
2024-11-20 10:33:36 +0800

    add

---
+++ gtw/internal/logic/file/getfilelogic.go
@@ -0,0 +1,31 @@
+package file
+
+import (
+	"context"
+
+	"zero-service/gtw/internal/svc"
+	"zero-service/gtw/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetFileLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 下载文件
+func NewGetFileLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetFileLogic {
+	return &GetFileLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GetFileLogic) GetFile(req *types.GetFileReq) error {
+	// todo: add your logic here and delete this line
+
+	return nil
+}
