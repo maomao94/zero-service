68e8c8ba646e8c6d3c66b124486af0f0afe39277
hehanpeng <hehanpengyy@163.com>
2024-11-20 14:35:13 +0800

    add

---
+++ common/tool/tool.go
@@ -0,0 +1,38 @@
+package tool
+
+import (
+	"fmt"
+	"github.com/shopspring/decimal"
+)
+
+var oneHundredDecimal decimal.Decimal = decimal.NewFromInt(100)
+
+// 分转元
+func Fen2Yuan(fen int64) float64 {
+	y, _ := decimal.NewFromInt(fen).Div(oneHundredDecimal).Truncate(2).Float64()
+	return y
+}
+
+// 元转分
+func Yuan2Fen(yuan float64) int64 {
+	f, _ := decimal.NewFromFloat(yuan).Mul(oneHundredDecimal).Truncate(0).Float64()
+	return int64(f)
+}
+
+func FormatFileSize(size int64) string {
+	const (
+		KB = 1024
+		MB = KB * 1024
+		GB = MB * 1024
+	)
+	switch {
+	case size >= GB:
+		return fmt.Sprintf("%.2f GB", float64(size)/float64(GB))
+	case size >= MB:
+		return fmt.Sprintf("%.2f MB", float64(size)/float64(MB))
+	case size >= KB:
+		return fmt.Sprintf("%.2f KB", float64(size)/float64(KB))
+	default:
+		return fmt.Sprintf("%d bytes", size)
+	}
+}
