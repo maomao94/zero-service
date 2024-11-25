c85b232e5ea920ef7d61a09ac5d3168bb9a5a73c
hehanpeng <hehanpengyy@163.com>
2024-11-25 16:28:33 +0800

    add iec 104 服务

---
+++ app/iecServer/iecserver.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"zero-service/app/iecServer/iecServer"
+	"zero-service/app/iecServer/internal/config"
+	"zero-service/app/iecServer/internal/server"
+	"zero-service/app/iecServer/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/iecserver.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		iecServer.RegisterIecServerServer(grpcServer, server.NewIecServerServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
