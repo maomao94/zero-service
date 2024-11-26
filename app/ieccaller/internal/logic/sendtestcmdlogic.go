c4fee9b6151aff098e0e7f02ecbd9b53b3653a19
hehanpeng <hehanpengyy@163.com>
2024-11-26 16:29:30 +0800

    add iec 104 服务

--- app/ieccaller/internal/logic/sendtestcmdlogic.go
+++ app/ieccaller/internal/logic/sendtestcmdlogic.go
@@ -2,16 +2,16 @@ package logic
 
 import (
 	"context"
-	"fmt"
+	"github.com/wendy512/go-iecp5/asdu"
 	"github.com/wendy512/go-iecp5/cs104"
+	"github.com/zeromicro/go-zero/core/executors"
+	"github.com/zeromicro/go-zero/core/logx"
 	"time"
 	"zero-service/app/ieccaller/ieccaller"
 	"zero-service/app/ieccaller/internal/iec"
 	"zero-service/app/ieccaller/internal/svc"
 	"zero-service/iec104"
 	"zero-service/iec104/iec104client"
-
-	"github.com/zeromicro/go-zero/core/logx"
 )
 
 type SendTestCmdLogic struct {
@@ -33,26 +33,38 @@ func (l *SendTestCmdLogic) SendTestCmd(in *ieccaller.SendTestCmdReq) (*ieccaller
 
 	option := cs104.NewOption()
 	if err = option.AddRemoteServer("127.0.0.1:2404"); err != nil {
-		panic(err)
+		return nil, err
 	}
-
+	option.SetParams(asdu.ParamsWide)
 	handler := &iec104client.ClientHandler{Call: &iec.ClientCall{}}
-
 	client := cs104.NewClient(handler, option)
-
 	client.LogMode(true)
 	client.SetLogProvider(iec104.NewLogProvider())
-
 	client.SetOnConnectHandler(func(c *cs104.Client) {
-		//c.SendStartDt() // 发送startDt激活指令
+		// 连接成功以后做的操作
+		l.Logger.Infof("connected %s iec104 server", "127.0.0.1:2404")
+		c.SendStartDt()
+	})
+	client.SetServerActiveHandler(func(c *cs104.Client) {
+		// 连接成功以后做的操作
+		l.Logger.Infof("server active %s iec104 server", "IP_ADDRESS:2404")
+		coa := asdu.CauseOfTransmission{
+			IsTest:     false,
+			IsNegative: false,
+			Cause:      asdu.Activation,
+		}
+		err = client.InterrogationCmd(coa, asdu.CommonAddr(1), asdu.QOIStation)
+		if err != nil {
+			l.Logger.Errorf("interrogation cmd error: %s", err.Error())
+		}
 	})
 	err = client.Start()
 	if err != nil {
-		panic(fmt.Errorf("Failed to connect. error:%v\n", err))
+		return nil, err
 	}
-
-	for {
-		time.Sleep(time.Second * 100)
-	}
+	executors.NewDelayExecutor(func() {
+		client.SendStopDt()
+		client.Close()
+	}, 5*time.Second).Trigger()
 	return &ieccaller.SendTestCmdRes{}, nil
 }
