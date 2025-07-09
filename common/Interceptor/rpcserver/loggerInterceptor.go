3df1c7b239f5b8cac6216215b30cff340e9115d9
maomao94 <hehanpengyy@163.com>
2025-07-09 11:38:53 +0800

    fix java 采用桥接元数据不需要打印

--- common/Interceptor/rpcserver/loggerInterceptor.go
+++ common/Interceptor/rpcserver/loggerInterceptor.go
@@ -2,27 +2,25 @@ package interceptor
 
 import (
 	"context"
-	"github.com/duke-git/lancet/v2/convertor"
 	"github.com/zeromicro/go-zero/core/logx"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
 )
 
 func LoggerInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		md = metadata.MD{}
-	}
-	traceid := md.Get("x-b3-traceid")
-	if traceid != nil {
-		traceId := convertor.ToString(traceid[0])
-		logx.WithContext(ctx).Infof("x-b3-traceid-%s", traceId)
-	}
-	spanid := md.Get("x-b3-spanid")
-	if spanid != nil {
-		spanId := convertor.ToString(spanid[0])
-		logx.WithContext(ctx).Infof("x-b3-spanid-%s", spanId)
-	}
+	//md, ok := metadata.FromIncomingContext(ctx)
+	//if !ok {
+	//	md = metadata.MD{}
+	//}
+	//traceid := md.Get("x-b3-traceid")
+	//if traceid != nil {
+	//	traceId := convertor.ToString(traceid[0])
+	//	logx.WithContext(ctx).Infof("x-b3-traceid-%s", traceId)
+	//}
+	//spanid := md.Get("x-b3-spanid")
+	//if spanid != nil {
+	//	spanId := convertor.ToString(spanid[0])
+	//	logx.WithContext(ctx).Infof("x-b3-spanid-%s", spanId)
+	//}
 	resp, err = handler(ctx, req)
 	if err != nil {
 		logx.WithContext(ctx).Errorf("【RPC-SRV-ERR】 %+v", err)
