7b1123a0ee4c9ee2795adeb89d1fdb0d87d2139f
maomao94 <hehanpengyy@163.com>
2025-09-04 12:09:00 +0800

    add

--- common/lalx/laltype.go
+++ common/lalx/laltype.go
@@ -1,22 +1,38 @@
 package lalx
 
-// 流分组数据结构
+// 流分组数据结构 - 根据实际JSON格式更新
 type GroupData struct {
-	StreamName   string         `json:"stream_name"`
-	CreateTimeMs int64          `json:"create_time_ms"`
-	Publisher    *SessionInfo   `json:"publisher"`
-	Subscribers  []*SessionInfo `json:"subscribers"`
-	Pullers      []*SessionInfo `json:"pullers"`
+	StreamName    string         `json:"stream_name"`
+	AppName       string         `json:"app_name"`
+	AudioCodec    string         `json:"audio_codec"`
+	VideoCodec    string         `json:"video_codec"`
+	VideoWidth    int            `json:"video_width"`
+	VideoHeight   int            `json:"video_height"`
+	Pub           *SessionInfo   `json:"pub"`
+	Subs          []*SessionInfo `json:"subs"`
+	Pull          *SessionInfo   `json:"pull"`
+	Pushs         []interface{}  `json:"pushs"`
+	InFramePerSec []FrameData    `json:"in_frame_per_sec"`
 }
 
-// 会话信息数据结构
+// 会话信息数据结构 - 根据实际JSON格式更新
 type SessionInfo struct {
-	SessionId    string `json:"session_id"`
-	Type         string `json:"type"`
-	ClientIp     string `json:"client_ip"`
-	CreateTimeMs int64  `json:"create_time_ms"`
-	SendBytes    int64  `json:"send_Bytes"`
-	RecvBytes    int64  `json:"recv_Bytes"`
+	SessionId         string `json:"session_id"`
+	Protocol          string `json:"protocol"`
+	BaseType          string `json:"base_type"`
+	StartTime         string `json:"start_time"`
+	RemoteAddr        string `json:"remote_addr"`
+	ReadBytesSum      int64  `json:"read_bytes_sum"`
+	WroteBytesSum     int64  `json:"wrote_bytes_sum"`
+	BitrateKbits      int    `json:"bitrate_kbits"`
+	ReadBitrateKbits  int    `json:"read_bitrate_kbits"`
+	WriteBitrateKbits int    `json:"write_bitrate_kbits"`
+}
+
+// 帧率数据结构
+type FrameData struct {
+	UnixSec int64 `json:"unix_sec"`
+	V       int   `json:"v"`
 }
 
 // 服务器信息数据结构
