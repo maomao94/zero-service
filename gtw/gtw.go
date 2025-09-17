300fb3df62f71c1a1ed6be888b88651b629fbd22
maomao94 <hehanpengyy@163.com>
2025-09-17 16:22:43 +0800

    add

--- gtw/gtw.go
+++ gtw/gtw.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
-	"github.com/zeromicro/go-zero/gateway"
 	"github.com/zeromicro/go-zero/rest"
 	"github.com/zeromicro/go-zero/rest/httpx"
 
@@ -29,22 +28,30 @@ func main() {
 	conf.MustLoad(*configFile, &c)
 
 	// grpc-gateway
-	server := gateway.MustNewServer(c.GatewayConf, func(server *gateway.Server) {
-		server.Use(rest.ToMiddleware(func(next http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				//r.Header.Set("Grpc-Metadata-user-id", "test")
-				origin := r.Header.Get("Origin")
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-				w.Header().Add("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id")
-				w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
-				w.Header().Add("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-				w.Header().Add("Access-Control-Allow-Credentials", "true")
-				next.ServeHTTP(w, r)
-			})
-		}))
-	})
+	//server := gateway.MustNewServer(c.GatewayConf, func(server *gateway.Server) {
+	//	server.Use(rest.ToMiddleware(func(next http.Handler) http.Handler {
+	//		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//			//r.Header.Set("Grpc-Metadata-user-id", "test")
+	//			origin := r.Header.Get("Origin")
+	//			w.Header().Set("Access-Control-Allow-Origin", origin)
+	//			w.Header().Add("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id")
+	//			w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
+	//			w.Header().Add("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+	//			w.Header().Add("Access-Control-Allow-Credentials", "true")
+	//			next.ServeHTTP(w, r)
+	//		})
+	//	}))
+	//})
+
+	server := rest.MustNewServer(c.RestConf, rest.WithCustomCors(func(header http.Header) {
+		header.Set("Access-Control-Allow-Origin", "*")
+		header.Add("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id")
+		header.Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
+		header.Add("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		header.Add("Access-Control-Allow-Credentials", "true")
+	}, nil, "*"))
 	ctx := svc.NewServiceContext(c)
-	handler.RegisterHandlers(server.Server, ctx)
+	handler.RegisterHandlers(server, ctx)
 	serviceGroup := service.NewServiceGroup()
 	defer serviceGroup.Stop()
 	serviceGroup.Add(server)
