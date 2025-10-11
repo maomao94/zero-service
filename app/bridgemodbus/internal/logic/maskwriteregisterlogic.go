83122515ec94b69e2632358c31a5ac0c98b0ead9
maomao94 <hehanpengyy@163.com>
2025-10-11 09:50:48 +0800

    feat(modbus): 实现屏蔽写保持寄存器功能

--- app/bridgemodbus/internal/logic/maskwriteregisterlogic.go
+++ app/bridgemodbus/internal/logic/maskwriteregisterlogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"zero-service/common/modbusx"
 
 	"zero-service/app/bridgemodbus/bridgemodbus"
 	"zero-service/app/bridgemodbus/internal/svc"
@@ -25,7 +28,31 @@ func NewMaskWriteRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 
 // 屏蔽写保持寄存器 (Function Code 0x16)
 func (l *MaskWriteRegisterLogic) MaskWriteRegister(in *bridgemodbus.MaskWriteRegisterReq) (*bridgemodbus.MaskWriteRegisterRes, error) {
-	// todo: add your logic here and delete this line
+	var mdCliPool *modbusx.ModbusClientPool
+	var err error
+	if len(in.ModbusCode) == 0 {
+		mdCliPool = l.svcCtx.ModbusClientPool
+	} else {
+		var ok bool
+		mdCliPool, ok = l.svcCtx.Manager.GetPool(in.ModbusCode) // 关键：用=而不是:=，避免局部变量
+		if !ok {
+			mdCliPool, err = l.svcCtx.AddPool(l.ctx, in.ModbusCode)
+			if err != nil {
+				return nil, fmt.Errorf("创建Modbus连接池失败: %w", err)
+			}
+		}
+		if mdCliPool == nil {
+			return nil, errors.New("获取的Modbus连接池为空")
+		}
+	}
+	mbCli := mdCliPool.Get()
+	defer mdCliPool.Put(mbCli)
 
-	return &bridgemodbus.MaskWriteRegisterRes{}, nil
+	results, err := mbCli.MaskWriteRegister(l.ctx, uint16(in.Address), uint16(in.AndMask), uint16(in.OrMask))
+	if err != nil {
+		return nil, err
+	}
+	return &bridgemodbus.MaskWriteRegisterRes{
+		Results: results,
+	}, nil
 }
