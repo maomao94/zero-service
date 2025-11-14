5bebfa05bab939258aa01b3ac40517c78efb953d
maomao94 <hehanpengyy@163.com>
2025-11-14 09:32:58 +0800

    feat(nacos): 集成nacos服务注册功能

--- app/bridgemqtt/bridgemqtt.go
+++ app/bridgemqtt/bridgemqtt.go
@@ -9,8 +9,11 @@ import (
 	"zero-service/app/bridgemqtt/internal/svc"
 	interceptor "zero-service/common/Interceptor/rpcserver"
 	_ "zero-service/common/carbonx"
+	"zero-service/common/nacosx"
 	_ "zero-service/common/nacosx"
 
+	"github.com/duke-git/lancet/v2/strutil"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
@@ -35,7 +38,25 @@ func main() {
 			reflection.Register(grpcServer)
 		}
 	})
-
+	// register service to nacos
+	if c.NacosConfig.IsRegister {
+		sc := []constant.ServerConfig{
+			*constant.NewServerConfig(c.NacosConfig.Host, c.NacosConfig.Port),
+		}
+		cc := &constant.ClientConfig{
+			NamespaceId:         c.NacosConfig.NamespaceId,
+			Username:            c.NacosConfig.Username,
+			Password:            c.NacosConfig.PassWord,
+			TimeoutMs:           5000,
+			NotLoadCacheAtStart: true,
+		}
+		m := map[string]string{
+			"gRPC_port":                 strutil.After(c.RpcServerConf.ListenOn, ":"),
+			"preserved.register.source": "go-zero",
+		}
+		opts := nacosx.NewNacosConfig(c.NacosConfig.ServiceName, c.ListenOn, sc, cc, nacosx.WithMetadata(m))
+		_ = nacosx.RegisterService(opts)
+	}
 	s.AddUnaryInterceptors(interceptor.LoggerInterceptor)
 	logx.AddGlobalFields(logx.Field("app", c.Name))
 	defer s.Stop()
--- app/bridgemqtt/internal/config/config.go
+++ app/bridgemqtt/internal/config/config.go
@@ -8,6 +8,15 @@ import (
 
 type Config struct {
 	zrpc.RpcServerConf
+	NacosConfig struct {
+		IsRegister  bool
+		Host        string
+		Port        uint64
+		Username    string
+		PassWord    string
+		NamespaceId string
+		ServiceName string
+	} `json:",optional"`
 	MqttConfig      mqttx.MqttConfig
 	StreamEventConf zrpc.RpcClientConf
 }
