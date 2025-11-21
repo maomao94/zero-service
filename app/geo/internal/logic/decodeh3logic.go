816b3beb464d1925a581aa5542e9321078f8712b
maomao94 <hehanpengyy@163.com>
2025-11-21 11:12:31 +0800

    feat(geo): 添加 H3 编码和解码功能

--- app/geo/internal/logic/decodeh3logic.go
+++ app/geo/internal/logic/decodeh3logic.go
@@ -2,10 +2,12 @@ package logic
 
 import (
 	"context"
+	"errors"
 
 	"zero-service/app/geo/geo"
 	"zero-service/app/geo/internal/svc"
 
+	"github.com/uber/h3-go/v4"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -25,7 +27,30 @@ func NewDecodeH3Logic(ctx context.Context, svcCtx *svc.ServiceContext) *DecodeH3
 
 // 解码 h3
 func (l *DecodeH3Logic) DecodeH3(in *geo.DecodeH3Req) (*geo.DecodeH3Res, error) {
-	// todo: add your logic here and delete this line
-
-	return &geo.DecodeH3Res{}, nil
+	if in.H3Index == "" {
+		return nil, errors.New("参数错误")
+	}
+	cell := h3.Cell(h3.IndexFromString(in.H3Index))
+	latLng, err := h3.CellToLatLng(cell)
+	if err != nil {
+		return nil, err
+	}
+	b, err := cell.Boundary()
+	if err != nil {
+		return nil, err
+	}
+	boundary := make([]*geo.Point, len(b))
+	for i, v := range b {
+		boundary[i] = &geo.Point{
+			Lat: v.Lat,
+			Lon: v.Lng,
+		}
+	}
+	return &geo.DecodeH3Res{
+		Center: &geo.Point{
+			Lat: latLng.Lat,
+			Lon: latLng.Lng,
+		},
+		Boundary: boundary,
+	}, nil
 }
