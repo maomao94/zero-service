9bc5bbefa856c83d9640720a64e37485254465a0
maomao94 <hehanpengyy@163.com>
2025-11-21 14:44:19 +0800

    feat(gis): 实现点是否在电子围栏内的判断逻辑

--- app/gis/internal/logic/pointinfenceslogic.go
+++ app/gis/internal/logic/pointinfenceslogic.go
@@ -2,9 +2,12 @@ package logic
 
 import (
 	"context"
+	"strconv"
 	"zero-service/app/gis/gis"
 	"zero-service/app/gis/internal/svc"
 
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/planar"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -24,7 +27,40 @@ func NewPointInFencesLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Poi
 
 // 点是否命中电子围栏（多个围栏）
 func (l *PointInFencesLogic) PointInFences(in *gis.PointInFencesReq) (*gis.PointInFencesRes, error) {
-	// todo: add your logic here and delete this line
+	if err := ValidatePoints(in.Point); err != nil {
+		return nil, err
+	}
 
-	return &gis.PointInFencesRes{}, nil
+	hitFenceIds := make([]string, 0)
+	point := orb.Point{in.Point.Lon, in.Point.Lat}
+
+	for key, fence := range in.Fences {
+		if len(fence.Points) == 0 && fence.Id == "" {
+			continue
+		}
+
+		var polygon orb.Polygon
+		var err error
+		if len(fence.Points) > 0 {
+			polygon, err = pbPointToOrbPolygon(fence.Points)
+			if err != nil {
+				l.Logger.Error("构建多边形失败, fenceId=", fence.Id, err)
+				continue
+			}
+		} else if fence.Id != "" {
+			// TODO: 从数据库/缓存加载多边形
+			continue
+		}
+
+		if planar.PolygonContains(polygon, point) {
+			if len(fence.Id) == 0 {
+				fence.Id = strconv.Itoa(key)
+			}
+			hitFenceIds = append(hitFenceIds, fence.Id)
+		}
+	}
+
+	return &gis.PointInFencesRes{
+		HitFenceIds: hitFenceIds,
+	}, nil
 }
