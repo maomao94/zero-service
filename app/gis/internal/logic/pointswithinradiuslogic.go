220984495b8c1fc0a3a452e6e7b07c2064ffa8b3
maomao94 <hehanpengyy@163.com>
2025-11-21 16:36:13 +0800

    feat(gis): 使用 MapReduce 优化点在半径内计算逻辑

--- app/gis/internal/logic/pointswithinradiuslogic.go
+++ app/gis/internal/logic/pointswithinradiuslogic.go
@@ -9,6 +9,7 @@ import (
 	"github.com/paulmach/orb"
 	"github.com/paulmach/orb/geo"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/mr"
 )
 
 type PointsWithinRadiusLogic struct {
@@ -32,15 +33,43 @@ func (l *PointsWithinRadiusLogic) PointsWithinRadius(in *gis.PointsWithinRadiusR
 		return nil, err
 	}
 	center := orb.Point{in.Center.Lon, in.Center.Lat}
-	hitIndexs := make([]int32, 0)
-	for i, p := range in.Points {
-		orbP := orb.Point{p.Lon, p.Lat}
-		distance := geo.Distance(center, orbP)
-		if distance <= in.RadiusMeters {
-			hitIndexs = append(hitIndexs, int32(i))
-		}
+	//hitIndexs := make([]int32, 0)
+	//for i, p := range in.Points {
+	//	orbP := orb.Point{p.Lon, p.Lat}
+	//	distance := geo.Distance(center, orbP)
+	//	if distance <= in.RadiusMeters {
+	//		hitIndexs = append(hitIndexs, int32(i))
+	//	}
+	//}
+	type indexedPoint struct {
+		idx int32
+		pt  orb.Point
+	}
+	hits, err := mr.MapReduce[indexedPoint, int32, []int32](
+		func(source chan<- indexedPoint) { // generate
+			for i, p := range in.Points {
+				source <- indexedPoint{
+					idx: int32(i),
+					pt:  orb.Point{p.Lon, p.Lat},
+				}
+			}
+		},
+		func(p indexedPoint, writer mr.Writer[int32], cancel func(error)) { // mapper
+			if geo.Distance(center, p.pt) <= in.RadiusMeters {
+				writer.Write(p.idx)
+			}
+		}, func(pipe <-chan int32, writer mr.Writer[[]int32], cancel func(error)) {
+			var result []int32
+			for idx := range pipe {
+				result = append(result, idx)
+			}
+			writer.Write(result)
+		},
+	)
+	if err != nil {
+		return nil, err
 	}
 	return &gis.PointsWithinRadiusRes{
-		HitIndexes: hitIndexs,
+		HitIndexes: hits,
 	}, nil
 }
