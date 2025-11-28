660a4d742695040a9e86585edd167b8dfa4cf0ce
maomao94 <hehanpengyy@163.com>
2025-11-28 14:39:33 +0800

    fix

--- app/iecstash/iecstash.go
+++ app/iecstash/iecstash.go
@@ -41,7 +41,7 @@ func main() {
 	ctx := svc.NewServiceContext(c)
 
 	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
-		iecstash.RegisterIecStashRpcServer(grpcServer, server.NewIecStashRpcServer(ctx))
+		iecstash.RegisterIecStashServer(grpcServer, server.NewIecStashServer(ctx))
 
 		if c.Mode == service.DevMode || c.Mode == service.TestMode {
 			reflection.Register(grpcServer)
