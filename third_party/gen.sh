23b9ad3e7e3e19dd9fa6076c9ab2988d23ff4893
maomao94 <hehanpengyy@163.com>
2026-02-05 10:22:45 +0800

    add

--- third_party/gen.sh
+++ third_party/gen.sh
@@ -18,7 +18,19 @@ protoc \
 # 移动到预期目录
 mv ${OUT_DIR}/zero-service/third_party/extproto ${OUT_DIR}/extproto
 
+echo "生成完成，已移动至 ${OUT_DIR}/extproto"
+
+protoc \
+  --proto_path=. \
+  --proto_path=${OUT_DIR} \
+  --go_out=${OUT_DIR} \
+  --go-grpc_out=${OUT_DIR} \
+  dji_error_code.proto
+
+mv ${OUT_DIR}/zero-service/third_party/dji_error_code ${OUT_DIR}/dji_error_code
+
+echo "生成完成，已移动至 ${OUT_DIR}/dji_error_code"
+
 # 删除多余目录
 rm -rf ${OUT_DIR}/zero-service
-
-echo "生成完成，已移动至 ${OUT_DIR}/extproto"
+echo "删除多余目录完成"
